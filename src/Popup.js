2bbd386dce25e019bcc3b01455ea6eed26c5bd7b
Curabet Guillaume <guillaume.curabet@gmail.com>
2021-09-15 21:33:54 +0200

    Update Display of Popup

--- src/Popup.js
+++ src/Popup.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import style from './popup.module.css'
 
-export default function PopupComponent({ type, text, boutons }) {
+export default function PopupComponent({ type, Text, boutons }) {
   return (
     <div className={style.popup + ' ' + style[type]}>
-      <div className={style.text}>{text}</div>
+      <div className={style.text}>
+        <Text />
+      </div>
       <div className={style.buttonPopup}>
         {boutons.map((Bouton, index) => {
           return (
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import PopupComponent from './Popup'
 
 export const Popup = ({ text, type, buttons }) => {
-  return <PopupComponent type={type} text={text} boutons={buttons} />
+  return <PopupComponent type={type} Text={text} boutons={buttons} />
 }
